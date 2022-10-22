213c3dc916cda1c5d4635cff694755363de16c38
Tobias Ruano <tobias@mobilenik.com>
2022-10-22 16:27:28 -0300

    Se primera pantalla de onboarding

---
+++ app/src/main/java/com/example/sip1/OnboardingFirst.java
@@ -0,0 +1,14 @@
+package com.example.sip1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnboardingFirst extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding_first);
+    }
+}
