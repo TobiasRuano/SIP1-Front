723e9bc1bb85d5a77ba7399f4af3c4ff5bcefd8b
TomasCanevaro <tomasjaviercanevaro@gmail.com>
2022-10-30 20:18:55 -0300

    Cree la pantalla Nuevo Gasto, faltan unos cambios

---
+++ app/src/main/java/com/example/sip1/NuevoGasto.java
@@ -0,0 +1,14 @@
+package com.example.sip1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NuevoGasto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_gasto);
+    }
+}
