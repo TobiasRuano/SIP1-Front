99d58c7e97b9424fd9fe40aea83c5e49e0021fc9
Alejandro Gabriel Rodriguez Castro <alejandrogrc@gmail.com>
2022-12-12 14:31:32 -0300

    Iconos de tutorial agregados

---
+++ app/src/main/java/com/example/sip1/GuideOne.java
@@ -0,0 +1,14 @@
+package com.example.sip1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuideOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide_one);
+    }
+}
