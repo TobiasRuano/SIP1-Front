5632303572ec4cab9d75a14a03d449403136aa66
Alejandro Gabriel Rodriguez Castro <alejandrogrc@gmail.com>
2022-12-12 14:33:20 -0300

    Pantalla 2 de tutorial

--- app/src/main/java/com/example/sip1/GuideTwo.java
+++ app/src/main/java/com/example/sip1/GuideTwo.java
@@ -2,13 +2,24 @@ package com.example.sip1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GuideTwo extends AppCompatActivity {
-
+    Button continueButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide_two);
+        continueButton = findViewById(R.id.bt_guide_two);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GuideTwo.this, OnboardingSecond.class); //change
+                startActivity(intent);
+            }
+        });
     }
 }
